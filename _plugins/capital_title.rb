95be6dc34ceb92d39296c2c6dc7095d2ff547d5a
Alessandro Surace <zioalex@gmail.com>
2024-04-13 10:29:30 +0200

    Capitalize llm

--- _plugins/capital_title.rb
+++ _plugins/capital_title.rb
@@ -1,7 +1,10 @@
 module Jekyll
   module Utils
     def titleize_slug(slug)
-      slug.split(/[_-]/).join(' ').capitalize
+      words_to_capitalize = ['llm']
+      slug.split(/[_-]/).map do |word|
+        words_to_capitalize.include?(word) ? word.capitalize : word
+      end.join(' ')
     end
   end
 end
